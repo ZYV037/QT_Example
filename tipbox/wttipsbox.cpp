951b06cf40f05820c0d2771ae596d5373aed852f
AaronZhang <461177348@qq.com>
2018-11-16 17:23:54 +0800

    Add files via upload

--- tipbox/wttipsbox.cpp
+++ tipbox/wttipsbox.cpp
@@ -5,52 +5,96 @@
 #include <QPushButton>
 #include <QHBoxLayout>
 
-TipBox::TipBox(QWidget* pParent):m_pParentWidget(pParent)
+TipBox::TipBox(QWidget* pParent):m_pParentWidget(pParent),m_bMouseIn(false)
 {
     setMaximumHeight(0);
     setMargin(0);
-    setStyleSheet("background-color:#00d8ff");
 
-     m_pTimeLine = new QTimeLine(10000, this);
-     connect(m_pTimeLine, SIGNAL(frameChanged(int)), this, SLOT(goTipBox(int)));
+    m_pTimeLine = new QTimeLine(10000, this);
+    m_pTimeLine->setFrameRange(0, 1000);
+    connect(m_pTimeLine, SIGNAL(frameChanged(int)), this, SLOT(goTipBox(int)));
 }
 
-void TipBox::ShowMessage(const QString& msg)
+void TipBox::ShowMessage(const QString& msg, TipType type)
 {
     setText(msg);
-    if( QTimeLine::Running == m_pTimeLine->state() )
+    setTextColor(type);
+
+    if( QTimeLine::NotRunning != m_pTimeLine->state() )
     {
         m_pTimeLine->stop();
     }
 
+
     m_pTimeLine->setDuration(10000);
-    m_pTimeLine->setFrameRange(0, 1000);
+    m_pTimeLine->setStartFrame(0);
     m_pTimeLine->start();
 }
 
-void TipBox::paintEvent(QPaintEvent *event)
+
+void TipBox::enterEvent(QEvent *event)
 {
-    QLabel::paintEvent(event);
+    m_bMouseIn = true;
+}
+
+void TipBox::leaveEvent(QEvent *event)
+{
+    m_pTimeLine->setDirection(QTimeLine::Direction::Forward);
+    m_bMouseIn = false;
+    if(m_pTimeLine->state() == QTimeLine::Paused )
+    {
+         m_pTimeLine->setPaused(false);
+    }
 }
 
 void TipBox::goTipBox(int value)
 {
-    if(value<=300)
-    {
-        this->setMaximumHeight(value/2);
-        m_pParentWidget->repaint();
-        return;
-    }
-    else if(value > 700)
-    {
-        this->setMaximumHeight((1000 - value)/2);
-        m_pParentWidget->repaint();
-    }
-
     if(value==1000)
     {
         m_pTimeLine->stop();
         this->setMaximumHeight(0);
+    }
+    else if(value <= 300)
+    {
+        this->setMaximumHeight(value/2);
         m_pParentWidget->repaint();
+    }
+    else if(value >= 700 )
+    {
+        if(m_bMouseIn)
+        {
+            m_pTimeLine->setDirection(QTimeLine::Direction::Backward);
+        }
+
+        this->setMaximumHeight((1000 - value)/2);
+        m_pParentWidget->repaint();
+    }
+    else if(m_bMouseIn && value == 500 )
+    {
+        m_pTimeLine->setDirection(QTimeLine::Direction::Forward);
+        m_pTimeLine->setPaused(true);
+    }
+
+    m_pParentWidget->repaint();
+    return;
+
+}
+
+void TipBox::setTextColor(TipType type)
+{
+    switch(type)
+    {
+        case Info:
+            setStyleSheet("color:black; background-color:#00d8ff");
+            break;
+        case Warning:
+            setStyleSheet("color:red; background-color:#00d8ff");
+            break;
+        case Error:
+            setStyleSheet("color:red; background-color:#00d8ff");
+            break;
+        case Fatal:
+            setStyleSheet("color:red; background-color:#00d8ff");
+            break;
     }
 }
--- tipbox/wttipsbox.h
+++ tipbox/wttipsbox.h
@@ -7,21 +7,31 @@ class QTimeLine;
 
 class TipBox: public QLabel
 {
+    enum TipType
+    {
+        Info,
+        Warning,
+        Error,
+        Fatal
+    };
+
     Q_OBJECT
 public:
     TipBox(QWidget* pParent);
 
-    void ShowMessage(const QString& msg);
-    void paintEvent(QPaintEvent *event);
-
+    void ShowMessage(const QString& msg, TipType type = Info);
+    void enterEvent(QEvent *event) override;
+    void leaveEvent(QEvent *event) override;
 private slots:
     void goTipBox(int value);
+    void setTextColor(TipType type);
 
 private:
     QTimeLine* m_pTimeLine;
     QString m_msg;
     QRect m_rect;
     QWidget* m_pParentWidget;
+    bool m_bMouseIn;
 };
 
 #endif // WTTIPSBOX_H
