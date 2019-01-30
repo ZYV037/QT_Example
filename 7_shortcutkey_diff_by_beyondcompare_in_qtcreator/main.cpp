3979ffb0d70e15a20aeea9fd4e0af427a37a3fef
ZYV037 <461177348@qq.com>
2019-01-30 17:52:39 +0800

    optime 7_shortcutkey_diff_by_beyondcompare_in_qtcreator

--- 7_shortcutkey_diff_by_beyondcompare_in_qtcreator/main.cpp
+++ 7_shortcutkey_diff_by_beyondcompare_in_qtcreator/main.cpp
@@ -6,9 +6,11 @@
 
 #pragma comment( linker, “/subsystem:windows /entry:mainCRTStartup” )
 
+//"C:\Program Files\TortoiseSVN\bin\svn_wrapper.exe" diff System/WMPS/DIS/UI/DisEventVariationUI/DisEventVariationUI.cpp
+//C:\WtgeoProducts: "C:\Program Files\TortoiseSVN\bin\svn_wrapper.exe" diff --internal-diff System/WMPS/App/DIS/DIS.pro
 int main(int argc, char *argv[])
 {
-    if(argc < 4)
+    if(argc < 3)
     {
         std::string cmd = "Cmd error: " ;
         for(int i = 0; i < argc; ++ i)
@@ -20,12 +22,30 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    std::string cmd = "TortoiseProc.exe /command:diff /path:";
-    cmd += argv[3];
-    int res = QProcess::execute(cmd.c_str());
+    std::string cmd1 = argv[1];
+    if( cmd1 != "diff" )
+    {
+        return -1;
+    }
+
+    if(argc == 3)
+    {
+        std::string cmd = "TortoiseProc.exe /command:diff /path:";
+        cmd += argv[2];
+        return QProcess::execute(cmd.c_str());
+    }
+
+    std::string cmd2 = argv[2];
+    if( cmd2 == "--internal-diff" )
+    {
+        std::string cmd = "TortoiseProc.exe /command:diff /path:";
+        cmd += argv[3];
+        return QProcess::execute(cmd.c_str());
+    }
+
 //    int res = system(cmd.c_str());
 
 //    std::cout << cmd << std::endl;
 
-    return res;
+    return -1;
 }
